mark-cooper/archivesspace

Add a new frontend test to catch missing dynamic enumeration translations

frontend/spec/spec_helper.rb
@@ -1,16 +1,5 @@
 require 'jsonmodel'
-
-JSONModel::init(:client_mode => false, :strict_mode => false)
+require 'frontend_enum_source'
+JSONModel::init(:client_mode => false, :strict_mode => false, :enum_source => FrontendEnumSource.new)
 include JSONModel
 
-RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
-
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  config.order = 'random'
-end

hudmol/archivesspace-1

frontend/spec/spec_helper.rb
@@ -1,16 +1,5 @@
 require 'jsonmodel'
-
-JSONModel::init(:client_mode => false, :strict_mode => false)
+require 'client_enum_source'
+JSONModel::init(:client_mode => false, :strict_mode => false, :enum_source => ClientEnumSource.new)
 include JSONModel
 
-RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
-
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  config.order = 'random'
-end