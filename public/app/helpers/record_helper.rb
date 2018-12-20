cf0ce37f3e8ebff40efa74ee46b779a1493cd793
Mark Custer <fordmadox@gmail.com>
2018-12-20 16:18:50 -0500

    iconography reset

--- public/app/helpers/record_helper.rb
+++ public/app/helpers/record_helper.rb
@@ -49,32 +49,36 @@ module RecordHelper
   end
 
   def icon_for_type(primary_type)
-    'fa ' + case primary_type
-              when 'repository'
-              'fa-home'
-              when  'resource'
-              'fa-archive'
-              when 'archival_object'
-              'fa-file-o'
-              when 'digital_object'
-              'fa-file-image-o'
-              when 'accession'
-              'fa-file-text-o'
-              when 'subject'
-              'fa-tag'
-              when  'agent_person'
-              'fa-user'
-              when 'agent_corporate_entity'
-              'fa-university'
-              when 'agent_family'
-              'fa-users'
-              when 'classification'
-              'fa-share-alt'
-              when 'top_container'
-              'fa-archive'
-              else
-              'fa-cog'
-            end
+    case primary_type
+        when 'repository'
+        'fa fa-home'
+        when  'resource'
+        'yc yc-collection'
+        when 'archival_object'
+        'fa fa-file-o'
+        when 'digital_object'
+        'fa fa-th'
+        when 'accession'
+        'fa fa-yale-accession'
+        when 'subject'
+        'fa fa-tag'
+        when  'agent_person'
+        'fa fa-user'
+        when 'agent_corporate_entity'
+        'fa fa-university'
+        when 'agent_family'
+        'fa fa-users'
+        when 'agent_software'
+        'fa fa-save'
+        when 'classification'
+        'fa fa-sitemap'
+        when 'top_container'
+        'fa fa-archive'
+        when 'digital_object_component'
+        'fa fa-th-large'
+        else
+        'fa fa-square'
+      end
   end
 
   def badge_for_type(primary_type)
