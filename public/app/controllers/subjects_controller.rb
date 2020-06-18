b4da807955c99ab50072237e85de75118c2e3f9d
Mark Custer <fordmadox@gmail.com>
2020-06-18 14:00:58 -0400

    Revert "deslug until slugs work with inline form requests"
    The plugin has been updated so that we don't need to worry about the URL rewriting bits there.
    
    This reverts commit f94d04d920cac08b6e45ecff0565be64c63c6a61.

--- public/app/controllers/subjects_controller.rb
+++ public/app/controllers/subjects_controller.rb
@@ -17,6 +17,10 @@ class SubjectsController <  ApplicationController
     :field => ['title']
   }
 
+  before_action(:only => [:show]) {
+    process_slug_or_id(params)
+  }
+  
   def index
     repo_id = params.fetch(:rid, nil)
     if !params.fetch(:q, nil)
