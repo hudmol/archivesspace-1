030b7bd3ee413f56a277a452b620d8b09f3f25ae
Payten Giles <payten.giles@gmail.com>
2021-04-13 13:43:25 +1000

    Piv#177553845 Add thumbnail to record read only pages

---
+++ frontend/app/helpers/thumbnail_helper.rb
@@ -0,0 +1,43 @@
+module ThumbnailHelper
+  def thumbnail_available?(record)
+    file_versions = fetch_file_versions(record)
+
+    !file_versions.empty?
+  end
+
+  def fetch_representative(file_versions)
+    file_versions.detect{|fv| !!fv['is_representative']}
+  end
+
+  def fetch_thumbnail(record)
+    file_versions = fetch_file_versions(record)
+
+    fetch_representative(file_versions) ||
+      ASUtils.wrap(file_versions).detect{|fv| fv['use_statement'] == 'image-thumbnail'} ||
+        ASUtils.wrap(file_versions).first
+  end
+
+  def file_version_is_image?(file_version)
+    # FIXME otherwise show an icon?
+    true
+  end
+
+  def fetch_file_versions(record)
+    result = []
+
+
+    if record['jsonmodel_type'] == 'archival_object'
+      record['instances'].each do |instance|
+        if instance['instance_type'] == 'digital_object'
+          result += ASUtils.wrap(instance['digital_object']['_resolved']['file_versions'])
+        end
+      end
+    elsif record['file_versions']
+      result = ASUtils.wrap(record['file_versions'])
+    elsif record['jsonmodel_type'] == 'file_version'
+      result << record
+    end
+
+    result
+  end
+end
