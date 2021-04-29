4783be275c9020059a86e2823650972c3fd4763b
Payten Giles <payten.giles@gmail.com>
2021-04-29 14:41:54 +1000

    Piv#177940161 provide thumbnails with better caption

--- frontend/app/helpers/thumbnail_helper.rb
+++ frontend/app/helpers/thumbnail_helper.rb
@@ -10,11 +10,13 @@ module ThumbnailHelper
   end
 
   def fetch_thumbnail(record)
-    file_versions = fetch_file_versions(record)
+    find_thumbnail_file_version(fetch_file_versions(record))
+  end
 
+  def find_thumbnail_file_version(file_versions)
     fetch_representative(file_versions) ||
       ASUtils.wrap(file_versions).detect{|fv| fv['use_statement'] == 'image-thumbnail'} ||
-        ASUtils.wrap(file_versions).first
+      ASUtils.wrap(file_versions).first
   end
 
   def file_version_is_image?(file_version)
@@ -38,19 +40,69 @@ module ThumbnailHelper
       record['instances'].each do |instance|
         if instance['instance_type'] == 'digital_object'
           if instance['is_representative']
-            return ASUtils.wrap(instance['digital_object']['_resolved']['file_versions'])
+            return ASUtils.wrap(instance['digital_object']['_resolved']['file_versions']).map{|fv|
+              fv.clone
+            }.map {|fv|
+              fv['_digital_object_title'] = instance['digital_object']['_resolved']['title']
+              fv
+            }
           else
-            result += ASUtils.wrap(instance['digital_object']['_resolved']['file_versions'])
+            result += ASUtils.wrap(instance['digital_object']['_resolved']['file_versions']).map{|fv|
+              fv.clone
+            }.map {|fv|
+              fv['_digital_object_title'] = instance['digital_object']['_resolved']['title']
+              fv
+            }
           end
         end
       end
     elsif record['file_versions']
-      result = ASUtils.wrap(record['file_versions'])
+      result = ASUtils.wrap(record['file_versions']).map{|fv|
+        fv.clone
+      }.map {|fv|
+        fv['_digital_object_title'] = record['title']
+        fv
+      }
     elsif record['jsonmodel_type'] == 'file_version'
-      result << record
+      result << record.clone
     end
 
     result
+  end
+
+
+  def caption_for_thumbnail(record, fallback_title = 'Thumbnail')
+    file_version_candidates = fetch_file_versions(record)
+
+    return fallback_title if file_version_candidates.empty?
+
+    # 1. take the representative caption
+    if (representative = file_version_candidates.detect{|fv| fv['is_representative']})
+      if representative['caption']
+        return representative['caption']
+      elsif representative['_digital_object_title']
+        return representative['_digital_object_title']
+      end
+    end
+
+    # 2. otherwise take the embedded thumbnail caption or digital object title
+    if (embed = find_thumbnail_file_version(file_version_candidates))
+      if embed['caption']
+        return embed['caption']
+      elsif embed['_digital_object_title']
+        return embed['_digital_object_title']
+      end
+    end
+
+    # 3. ok.. take the first digital object's title
+    file_version_candidates.each do |fv|
+      if fv['_digital_object_title']
+        return fv['_digital_object_title']
+      end
+    end
+
+    # 4. if all fails then take the fallback (current record's title)
+    fallback_title
   end
 
 
@@ -68,7 +120,10 @@ module ThumbnailHelper
       proc {|record|
         json = ASUtils.json_parse(record['json'])
         if thumbnail_available?(json)
-          render_aspace_partial(:partial => "digital_objects/thumbnail_for_file_version", :locals => {:file_version => fetch_thumbnail(json)})
+          render_aspace_partial(:partial => "digital_objects/thumbnail_for_file_version", :locals => {
+            :file_version => fetch_thumbnail(json),
+            :caption => caption_for_thumbnail(json, json['title'] || json['display_string'] || 'Thumbnail'),
+          })
         end
       },
       :sortable => false, :class => 'thumbnail-col'
